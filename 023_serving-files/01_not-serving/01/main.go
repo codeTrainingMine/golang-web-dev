14d714228474aa636bdabfc5e74f96ec24738ac0
codeTrainingMine <enhilex.linux@gmail.com>
2017-06-21 21:40:55 +1000

    adds files to 023

---
+++ 023_serving-files/01_not-serving/01/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main()  {
+	http.HandleFunc("/", dog)
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	io.WriteString(w, `
+	<--not serving from our server-->
+	<img src="https://upload.wikimedia.org/wikipedia/commons/9/93/Golden_Retriever_Carlos_%2810581910556%29.jpg">
+	`)
+}
