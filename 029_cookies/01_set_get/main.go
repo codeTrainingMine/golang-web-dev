6241055be4582f6d65049169151b52284995ba95
codeTrainingMine <enhilex.linux@gmail.com>
2017-07-01 22:47:26 +1000

    adds files to 029

---
+++ 029_cookies/01_set_get/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main()  {
+	http.HandleFunc("/", set)
+	http.HandleFunc("/read", read)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func set(w http.ResponseWriter, req *http.Request)  {
+	http.SetCookie(w, &http.Cookie{
+		Name: "my-cookie",
+		Value: "Some value",
+	})
+	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+	fmt.Fprintln(w, "in chrome go to: dev tools / application / cookies")
+}
+
+func read(w http.ResponseWriter, req *http.Request)  {
+	c, err := req.Cookie("my-cookie")
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "YOUR COOKIE:", c)
+}
