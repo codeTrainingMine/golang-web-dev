3e9fcf3ba4caaccff80181c32b3809faa865f919
codeTrainingMine <enhilex.linux@gmail.com>
2017-08-05 09:20:15 +1000

    adds files to 042

---
+++ 042_mongodb/05_mongodb/01_update-user-controller/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"golang-web-dev/042_mongodb/05_mongodb/01_update-user-controller/controllers"
+	"gopkg.in/mgo.v2"
+)
+
+func main()  {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *mgo.Session {
+	// Connect to our local mongo
+	s, err := mgo.Dial("mongodb://localhost")
+
+	// Check if connection erro, is mongo running?
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
